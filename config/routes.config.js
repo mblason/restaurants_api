d2bb79f4b1e178e51ea93a3c80e60ec938a9ed82
mblason <marina.blasongraviz@gmail.com>
2022-12-02 09:44:07 +0100

    auth google feature done and auth middleware added

--- config/routes.config.js
+++ config/routes.config.js
@@ -2,13 +2,17 @@ const router = require("express").Router();
 const passport = require("passport");
 const fileUploader = require("./cloudinary.config");
 const authController = require("../controllers/auth.controller");
-//const authMiddleware = require("../middlewares/auth.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
+
+const SCOPES = ["profile", "email"];
 
 // AUTH
 router.get("/", (req, res, next) => res.json({ ok: true }));
 router.post("/register", authController.register);
 router.get("/activate/:token", authController.activateAccount);
 router.post("/login", authController.login);
+router.get("/login/google", passport.authenticate("google-auth", { scope: SCOPES }));
+router.get("/auth/google/callback", authController.loginGoogle);
 
 // HOME
 
